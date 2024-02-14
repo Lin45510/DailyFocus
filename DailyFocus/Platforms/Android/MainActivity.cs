7813957e59a9d2b1c561320d1ffa733f76f7a27d
Y Lin N <152435819+Lin45510@users.noreply.github.com>
2024-02-14 09:19:45 -0300

    Final Release Version

--- DailyFocus/Platforms/Android/MainActivity.cs
+++ DailyFocus/Platforms/Android/MainActivity.cs
@@ -7,5 +7,11 @@ namespace DailyFocus
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(100, 178, 135, 248));
+
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
